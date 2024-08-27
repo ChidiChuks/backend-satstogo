18ad33d1b2fd76891449f4038506042e8eb622eb
naiyoma <lankas.aurelia@gmail.com>
2024-08-27 23:28:05 +0300

    socil login url setup

--- sattogo/urls.py
+++ sattogo/urls.py
@@ -18,9 +18,21 @@ from django.contrib import admin
 from django.urls import path,include
 from django.conf import settings
 from django.conf.urls.static import static
+from dj_rest_auth.views import LoginView, LogoutView
+from dj_rest_auth.registration.views import SocialLoginView
+from allauth.socialaccount.providers.google.provider import GoogleProvider
+from allauth.socialaccount.providers.oauth2.views import OAuth2LoginView, OAuth2CallbackView
+
+
+class GoogleLogin(SocialLoginView):
+    adapter_class = GoogleProvider.oauth2_adapter_class
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('auth/', include('dj_rest_auth.urls')),  # Login, Logout, Password Reset
+    path('auth/registration/', include('dj_rest_auth.registration.urls')),  # Registration
+    path('auth/social/google/', GoogleLogin.as_view(), name='google_login'),  # Google login
     path('api/', include('api.urls')),
     path('events/', include('events.urls')),
     path('wallet/', include('wallet.urls')),
